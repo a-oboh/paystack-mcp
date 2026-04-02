980f4710d796dca32ae3b5bd921efd2e726f2890
a-oboh <albertoboh7@gmail.com>
2026-04-02 02:37:09 +0100

    feature: implement rate limit with helmet

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const isDev = configService.get<string>('NODE_ENV') === 'development';
+
+    app.use(helmet());
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,6 +19,10 @@ async function bootstrap() {
       transform: true,       // auto-transform payloads to DTO types
     }),
   );
+
+  if (isDev) {
+    app.enableCors({ origin: 'http://localhost:3000' });
+  }
 
   const config = app.get(ConfigService);
   const port = config.get<number>('PORT', 3000);
